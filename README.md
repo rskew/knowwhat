351c0ff04abf5a24241681a3c1928e856fb64853
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2019-10-08 11:25:15 +1100

    Modernise and simplify build system
    This removes Bower, which is deprecated, in favour of using npm or yarn for getting source packages.

---
+++ README.md
@@ -0,0 +1,34 @@
+# Workflow
+
+Graph drawing for mindmapping, knowledge engineering and creative-tool-making.
+
+![example graph](./exampleGraph.png)
+
+## Quick start
+
+You will either need [NPM](https://www.npmjs.com/get-npm) or [Yarn](https://yarnpkg.com/) installed.
+
+### NPM
+
+```sh
+npm run dev     # run development server
+npm run build   # build production assets
+```
+
+### Yarn
+
+```sh
+yarn dev        # run development server
+yarn build      # build production assets
+```
+
+## Development
+
+We use [spago](https://github.com/spacchetti/spago) to manage our Purescript dependencies.
+While this is installed as a `dev-dependency` in `package.json` you may want to install it directly to make it easier to manage these dependencies directly.
+
+## License
+
+The project is licensed under the terms of the Apache License (Version 2.0).
+
+See [LICENSE](./LICENSE) or http://www.apache.org/licenses/LICENSE-2.0 for details.
--- src/GraphComponent.js
+++ src/GraphComponent.js
@@ -1,30 +1,32 @@
-var FileSaver = require("../../bower_components/file-saver/dist/FileSaver.min.js");
+var FileSaver = require("file-saver");
 
 //////
 // Save/loading graph
 
 // saveJSON :: String -> String -> Effect Unit
-var saveJSON = function (jsonStr) {
-    return function (fname) {
-        return function () {
-            var blob = new Blob([jsonStr], {type: "application/JSON;charset=utf-8"});
-            FileSaver.saveAs(blob, fname);
-        };
+var saveJSON = function(jsonStr) {
+  return function(fname) {
+    return function() {
+      var blob = new Blob([jsonStr], {
+        type: "application/JSON;charset=utf-8"
+      });
+      FileSaver.saveAs(blob, fname);
     };
+  };
 };
 
 // loadFile :: Effect Unit
 var loadFile = function() {
-    simulateClickOn(document.querySelector('input[type=file]'));
+  simulateClickOn(document.querySelector("input[type=file]"));
 };
 
-var simulateClickOn = function (element) {
-    var clickEvent = new MouseEvent('click', {
-        view: window,
-        bubbles: true,
-        cancelable: true
-    });
-    element.dispatchEvent(clickEvent);
+var simulateClickOn = function(element) {
+  var clickEvent = new MouseEvent("click", {
+    view: window,
+    bubbles: true,
+    cancelable: true
+  });
+  element.dispatchEvent(clickEvent);
 };
 
 //exports.saveJSON = saveJSON;
